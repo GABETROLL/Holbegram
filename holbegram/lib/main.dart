e61aae0abc194106030317a71aaada137c8c4c96
GABETROLL <gabriel7icplhappy@gmail.com>
2024-04-12 16:03:51 -0400

    Change main to test Home widget

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/upload_image_screen.dart';
+import 'screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const AddPicture(email: 'gabe@gmail.com', password: 'sjefweupoaiurep', username: 'gabetroll'),
+      home: const Home(),
     );
   }
 }
