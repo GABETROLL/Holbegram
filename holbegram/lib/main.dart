be0a3d8dd4b102619bad438d7e948f0e194e4470
GABETROLL <gabriel7icplhappy@gmail.com>
2024-04-14 20:41:03 -0400

    Add Firebase to app

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import '../screens/Pages/add_image.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
