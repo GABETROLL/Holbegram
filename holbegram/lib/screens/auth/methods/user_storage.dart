0b9e41e34300fa021679917d4c019e5db469844c
GABETROLL <gabriel7icplhappy@gmail.com>
2024-04-21 16:58:19 -0400

    Document StorageMethods, and have one global designated 'childName' location for storing images, in a variable

--- holbegram/lib/screens/auth/methods/user_storage.dart
+++ holbegram/lib/screens/auth/methods/user_storage.dart
@@ -5,10 +5,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class StorageMethods {
+  static const String childName = 'images';
+
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   /// Assumes that there's already a current user signed in in `FirebaseStorage.instance`!!!
+  /// Returns the image's download URL if everything goes well.
   Future<String> uploadImageToStorage(bool isPost, String childName, Uint8List file) async {
     Reference ref = _storage.ref().child(childName).child(_auth.currentUser!.uid);
     if (isPost) {
